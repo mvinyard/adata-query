d9c0d8677b78d29d09c9e294774f49784c6d2c50
mvinyard <mvinyard.ai@gmail.com>
2023-08-03 15:21:39 +0000

    tested dict functionality

--- adata_query/_core/_fetcher.py
+++ adata_query/_core/_fetcher.py
@@ -4,6 +4,7 @@ import ABCParse
 import autodevice
 import anndata
 import torch as _torch
+import numpy as np
 
 
 # -- import local dependencies: ------------------------------------------------
@@ -12,7 +13,7 @@ from ._formatter import format_data
 
 
 # -- set typing: ---------------------------------------------------------------
-from typing import Optional, Dict, List
+from typing import Dict, List, Optional, Union
 
 
 # -- operational class: --------------------------------------------------------
@@ -31,10 +32,12 @@ class AnnDataFetcher(ABCParse.ABCParse):
         return format_data(data=data, torch = self._torch, device = self._device)
 
     def _grouped_subroutine(self, adata, key):
-        for group, group_df in self._GROUPED:
-            if self._as_dict:
-                yield key, self._forward(adata[group_df.index], key)
-            yield self._forward(adata[group_df.index], key)
+        if self._as_dict:
+            for group, group_df in self._GROUPED:
+                yield group, self._forward(adata[group_df.index], key)
+        else:
+            for group, group_df in self._GROUPED:
+                yield self._forward(adata[group_df.index], key)
 
     def __call__(
         self,
@@ -77,7 +80,7 @@ class AnnDataFetcher(ABCParse.ABCParse):
 
         if hasattr(self, "_groupby"):
             if self._as_dict:
-                return dict(self._grouped_subgroutine(adata, key))
+                return dict(self._grouped_subroutine(adata, key))
             return list(self._grouped_subroutine(adata, key))
         return self._forward(adata, key)
 
@@ -90,55 +93,60 @@ def fetch(
     as_dict: bool = True,
     *args,
     **kwargs,
-) -> Union[torch.Tensor, np.ndarray, List[torch.Tensor], List[np.ndarray], Dict[torch.Tensor], Dict[np.ndarray]]:
+) -> Union[
+    _torch.Tensor,
+    np.ndarray,
+    List[Union[_torch.Tensor, np.ndarray]],
+    Dict[Union[str, int], Union[_torch.Tensor, np.ndarray]],
+]:
     """
     Given, adata and a key that points to a specific matrix stored in adata,
     return the data, formatted either as np.ndarray or torch.Tensor. If formatted
     as torch.Tensor, device may be specified based on available devices.
-    
+
     Parameters
     ----------
     adata: anndata.AnnData [ required ]
         Annotated single-cell data object.
-        
+
     key: str [ required ]
         Key to access a matrix in adata. For example, if you wanted to access
         adata.obsm['X_pca'], you would pass: "X_pca".
-    
+
     groupby: Optional[str], default = None
         Optionally, one may choose to group data according to a cell-specific
         annotation in adata.obs. This would invoke returning data as List
-        
+
     torch: bool, default = False
         Boolean indicator of whether data should be formatted as torch.Tensor. If
         False (default), data is formatted as np.ndarray.device (torch.device) =
-        autodevice.AutoDevice(). Should torch=True, the device ("cpu", "cuda:N", 
-        "mps:N") may be set. The default value, autodevice.AutoDevice() will 
+        autodevice.AutoDevice(). Should torch=True, the device ("cpu", "cuda:N",
+        "mps:N") may be set. The default value, autodevice.AutoDevice() will
         indicate the use of GPU, if available.
 
     as_dict: bool, default = True
         Only relevant when `groupby` is not None. Boolean indicator to return
         data in a Dict where the key for each value corresponds to the respective
         `groupby` value. If False, returns List.
-    
+
     Returns
     -------
-    data: Union[torch.Tensor, np.ndarray, List[torch.Tensor], List[np.ndarray], Dict[torch.Tensor], Dict[np.ndarray]]
+    data: Union[torch.Tensor, np.ndarray, List[Union[torch.Tensor, np.ndarray]], Dict[Union[str, int], Union[torch.Tensor, np.ndarray]]
         Formatted data as np.ndarray or torch.Tensor. If torch=True the torch.Tensor
-        is allocated to the device indicated by the device argument. If groupby is passed,
-        returned as a List[np.ndarray] or List[torch.Tensor]
+        is allocated to the device indicated by the device argument. If `groupby` is passed,
+        returned as Dict[np.ndarray] or Dict[torch.Tensor]. If groupby is passed and `as_dict`
+        = False, returns List[np.ndarray] or List[torch.Tensor].
     """
-    
-    
+
     fetcher = AnnDataFetcher()
-    
+
     return fetcher(
-        adata = adata,
-        key = key,
-        groupby = groupby,
-        torch = torch,
-        device = device,
-        as_dict = as_dict,
+        adata=adata,
+        key=key,
+        groupby=groupby,
+        torch=torch,
+        device=device,
+        as_dict=as_dict,
         *args,
         **kwargs,
     )
