318a4025f9a03654563d95e602bf637c05a8917e
Michael Vinyard <47393421+mvinyard@users.noreply.github.com>
2023-08-03 08:48:52 -0600

    Address #1 - adding `as_dict` arg

--- adata_query/_core/_fetcher.py
+++ adata_query/_core/_fetcher.py
@@ -12,7 +12,7 @@ from ._formatter import format_data
 
 
 # -- set typing: ---------------------------------------------------------------
-from typing import Optional
+from typing import Optional, Dict, List
 
 
 # -- operational class: --------------------------------------------------------
@@ -32,6 +32,8 @@ class AnnDataFetcher(ABCParse.ABCParse):
 
     def _grouped_subroutine(self, adata, key):
         for group, group_df in self._GROUPED:
+            if self._as_dict:
+                yield key, self._forward(adata[group_df.index], key)
             yield self._forward(adata[group_df.index], key)
 
     def __call__(
@@ -41,22 +43,41 @@ class AnnDataFetcher(ABCParse.ABCParse):
         groupby: Optional[str] = None,
         torch: bool = False,
         device: _torch.device = autodevice.AutoDevice(),
+        as_dict: bool = True,
     ):
         """
-        adata: anndata.AnnData [required]
+        adata: anndata.AnnData [ required ]
+            Annotated single-cell data object.
         
-        key: str [required]
+        key: str [ required ]
+            Key to access a matrix in adata. For example, if you wanted to access
+            adata.obsm['X_pca'], you would pass: "X_pca".
         
         groupby: Optional[str], default = None
-        
+            Optionally, one may choose to group data according to a cell-specific
+            annotation in adata.obs. This would invoke returning data as List
+            
         torch: bool, default = False
-        
+            Boolean indicator of whether data should be formatted as torch.Tensor. If
+            False (default), data is formatted as np.ndarray.device (torch.device) =
+            autodevice.AutoDevice(). Should torch=True, the device ("cpu", "cuda:N", 
+            "mps:N") may be set. The default value, autodevice.AutoDevice() will 
+            indicate the use of GPU, if available.
+
         device: torch.device, default = autodevice.AutoDevice()
+            
+    
+        as_dict: bool, default = True
+            Only relevant when `groupby` is not None. Boolean indicator to return
+            data in a Dict where the key for each value corresponds to the respective
+            `groupby` value. If False, returns List.
         """
 
         self.__update__(locals(), public=[None])
 
         if hasattr(self, "_groupby"):
+            if self._as_dict:
+                return dict(self._grouped_subgroutine(adata, key))
             return list(self._grouped_subroutine(adata, key))
         return self._forward(adata, key)
 
@@ -66,9 +87,10 @@ def fetch(
     groupby: Optional[str] = None,
     torch: bool = False,
     device: _torch.device = autodevice.AutoDevice(),
+    as_dict: bool = True,
     *args,
     **kwargs,
-):
+) -> Union[torch.Tensor, np.ndarray, List[torch.Tensor], List[np.ndarray], Dict[torch.Tensor], Dict[np.ndarray]]:
     """
     Given, adata and a key that points to a specific matrix stored in adata,
     return the data, formatted either as np.ndarray or torch.Tensor. If formatted
@@ -93,10 +115,15 @@ def fetch(
         autodevice.AutoDevice(). Should torch=True, the device ("cpu", "cuda:N", 
         "mps:N") may be set. The default value, autodevice.AutoDevice() will 
         indicate the use of GPU, if available.
+
+    as_dict: bool, default = True
+        Only relevant when `groupby` is not None. Boolean indicator to return
+        data in a Dict where the key for each value corresponds to the respective
+        `groupby` value. If False, returns List.
     
     Returns
     -------
-    data: Union[torch.Tensor, np.ndarray, List[torch.Tensor], List[np.ndarray]
+    data: Union[torch.Tensor, np.ndarray, List[torch.Tensor], List[np.ndarray], Dict[torch.Tensor], Dict[np.ndarray]]
         Formatted data as np.ndarray or torch.Tensor. If torch=True the torch.Tensor
         is allocated to the device indicated by the device argument. If groupby is passed,
         returned as a List[np.ndarray] or List[torch.Tensor]
@@ -111,6 +138,7 @@ def fetch(
         groupby = groupby,
         torch = torch,
         device = device,
+        as_dict = as_dict,
         *args,
         **kwargs,
     )
