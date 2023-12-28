82fc621455a7341570a87305696288f61393bdb1
Michael Vinyard <47393421+mvinyard@users.noreply.github.com>
2023-12-28 13:03:37 -0500

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -10,7 +10,7 @@ release = '0.1.0'
 import os
 import sys
 
-sys.path.insert(0, os.path.abspath('../../adata_query'))
+sys.path.insert(0, os.path.abspath('../../'))
 
 extensions = [
     'sphinx.ext.autodoc',
